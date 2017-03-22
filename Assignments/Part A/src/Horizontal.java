7b193227c02132f9710fd4b8ab198079cf41e2b0
Ammar Ahmed <ammar@Ammars-MacBook-Pro.local>
2017-03-22 19:02:50 +1100

    Resolving merge conflicts

--- Assignments/Part A/src/Horizontal.java
+++ Assignments/Part A/src/Horizontal.java
@@ -1,10 +1,6 @@
 /**
  * Created by Nam Nguyen on 15/03/2017.
  */
-<<<<<<< HEAD
-public class Horizontal
-{
-=======
 public class Horizontal extends Player{
 
     public static final boolean LEFT = false;
@@ -24,5 +20,4 @@ public class Horizontal extends Player{
     public void move(){
         //do something
     }
->>>>>>> 095bfa7a0b0a486bb890b7f1e6e6a2ebe165ebe6
 }
