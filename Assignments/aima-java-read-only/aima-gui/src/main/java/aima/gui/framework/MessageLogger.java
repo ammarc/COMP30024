9ec214ee95771af16beb29842705c416ab6896c9
ammarc <ammara@student.unimelb.edu.au>
2017-05-07 16:46:46 +1000

    Removing .idea and aima library

--- Assignments/aima-java-read-only/aima-gui/src/main/java/aima/gui/framework/MessageLogger.java
+++
@@ -1,9 +0,0 @@
-package aima.gui.framework;
-
-/**
- * Simple interface for message logging.
- * @author R. Lunde
- */
-public interface MessageLogger {
-	public void log(String message);
-}
--- Assignments/aima-java-read-only/aimax-osm/src/main/java/aimax/osm/data/entities/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains the domain objects. The representation of nodes and
- * ways is very close to the corresponding objects within the Open Street Map
- * data format. Only minor administrative attributes are ignored.
- * Relations are currently not supported.
- */
-package aimax.osm.data.entities;
